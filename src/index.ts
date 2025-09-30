98d5df8e1de06fda25dfba18e592f0c3dfcf6216
Dimab01112 <118934619+Dimab01112@users.noreply.github.com>
2025-09-30 18:33:06 +0000

    Add TypeScript setup with example

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+let username: string = "Дмитро";
+let age: number = 20;
+let isStudent: boolean = true;
+
+console.log(`Ім'я: ${username}`);
+console.log(`Вік: ${age}`);
+console.log(`Студент: ${isStudent}`);
